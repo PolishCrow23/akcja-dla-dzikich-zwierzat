91e6521d885e83db2868e1e9c01ea0fb3697a6c2
unknown <igaspodzieja00@gmail.com>
2024-04-29 15:10:29 +0200

    formatted date

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const postsCollection = defineCollection({
     schema: z.object({
         title: z.string(),
-        date: z.string(),
+        date: z.coerce.date(),
         image: z.string(),
     }),
 });
